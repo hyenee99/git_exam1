fde21274ac16f4483318c4a98a3b3035c928e406
Yoon, Hyewon <twoddal2@naver.com>
2021-09-28 09:38:34 +0900

    201814107-4

--- src/git_exam1/Hello.java
+++ src/git_exam1/Hello.java
@@ -3,9 +3,9 @@ package git_exam1;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("ITÀ¶ÇÕÀÚÀ²ÇĞºÎ");
-		System.out.println("201814107");
-		System.out.println("À±Çı¿ø");
+		System.out.println("¼Ò¼Ó: ITÀ¶ÇÕÀÚÀ²ÇĞºÎ");
+		System.out.println("ÇĞ¹ø: 201814107");
+		System.out.println("ÀÌ¸§: À±Çı¿ø");
 	}
 
 }
